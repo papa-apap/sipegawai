7a82d8f70cf9e8e4544e2fa37a9c3f0cc27860b7
Ray Azrin Karim <rayazrin19@gmail.com>
2020-11-23 10:41:55 +0700

    feat: Initiate project

---
+++ src/main/java/apap/tugas/sipegawai/SipegawaiApplication.java
@@ -0,0 +1,13 @@
+package apap.tugas.sipegawai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipegawaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipegawaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/tugas/sipegawai/SipegawaiApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugas.sipegawai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipegawaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
